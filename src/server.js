661ecb908140d0abb70b3f8b3a982bd585f16e9f
Peter Szilagyi <extern_peter.szilagyi@allianz.at>
2024-09-23 09:28:09 +0200

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("empty server");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hola World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
